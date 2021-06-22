e23e876826d3bc26b3dde13cf1d095c9ba5000cc
meghna <meghnabarn2001@gmail.com>
2021-06-22 14:53:58 +0530

    user restricted to add blog from his/her name only

--- yourblog/forms.py
+++ yourblog/forms.py
@@ -13,13 +13,15 @@ class PostForm(forms.ModelForm):
         model=Post
         fields=('title','title_tag','author', 'category' ,'body')
         widgets={
-            'title':forms.TextInput(attrs={'class':'form-control'}),
+            'title':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Your Blog Title'}),
             
-            'title_tag':forms.TextInput(attrs={'class':'form-control'}),
-            'author':forms.Select(attrs={'class':'form-control'}),
+            'title_tag':forms.TextInput(attrs={'class':'form-control' ,'placeholder':'Your Blog Title tag'}),
+            # 'author':forms.Select(attrs={'class':'form-control'}),
+            'author':forms.TextInput(attrs={'class':'form-control' , 'value': '' , 'id':'username' ,'type':'hidden'}),
+
             # 'category':forms.Select(choices= categories ,attrs={'class':'form-control'}),
             'category':forms.Select( choices=choice_list ,attrs={'class':'form-control'}),
-            'body':forms.Textarea(attrs={'class':'form-control'}),
+            'body':forms.Textarea(attrs={'class':'form-control','placeholder':'Your Blog Body'}),
         }
 
 class EditForm(forms.ModelForm):  
