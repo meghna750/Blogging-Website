56277b8355f0efdd330c54d19ba7ab9b5b857e9f
meghna <meghnabarn2001@gmail.com>
2021-07-05 09:50:38 +0530

    added profile page for eavery user, created showProfilePageView from profile model of yourblog

--- members/views.py
+++ members/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from django.views import generic
+from django.views.generic import DetailView
 from django.contrib.auth.forms import UserCreationForm , UserChangeForm , PasswordChangeForm
 from django.contrib.auth.views import  PasswordChangeView
 from django.urls import reverse_lazy 
 from .forms import SignUpForm , EditProfileForm , PasswordChangingForm
+from yourblog.models import profile
 # Create your views here.
+
+class ShowProfilePageView(DetailView): 
+    model=profile 
+    template_name='registration/user_profile.html'
+    def get_context_data(self,*args,**kwargs):
+        users=profile.objects.all() # it had only one obj - name, so we can call all () here
+        context=super(ShowProfilePageView,self).get_context_data()
+        page_user= get_object_or_404(profile,id=self.kwargs['pk'])
+        context["page_user"]=page_user
+        return context
+
 
 class UserRegisterView(generic.CreateView): 
     form_class= SignUpForm
